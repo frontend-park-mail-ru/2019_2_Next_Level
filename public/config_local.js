be0dded66acffe5f2bd99bc719f0d7db4dd49e0e
Ian <andreykochnov@yandex.ru>
2019-12-23 18:32:57 +0300

    two config and script

---
+++ public/config_local.js
@@ -0,0 +1,9 @@
+export const Config = {
+	// backend: 'http://localhost:3001',
+	// backend: 'https://nextlevel.hldns.ru',
+	// backend: 'https://nl-mail.ru',
+	backend: 'http://localhost:3000',
+	swActive: false,
+	// swActive: true,
+	messagesPerPage: 15,
+};
